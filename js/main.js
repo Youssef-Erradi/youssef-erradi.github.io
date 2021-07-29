f5308c2a819f000d3b2dabf59d08445f1b6da457
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2021-07-29 23:31:36 +0100

    finish menu logic

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 document.querySelectorAll("section").forEach((section, index)=>{
     section.style.backgroundColor = `rgb(23, 113, 241, ${index*0.15})`
 })
+const menu      = document.querySelector("#menu")
+const nav       = document.querySelector(".nav ul")
+const links     = document.querySelectorAll(".nav ul li")
+const container = document.querySelector(".container")
+console.log(container)
+menu.addEventListener("click", ()=>{
+    container.style = `overflow-y: ${nav.classList.toggle("active")?"hidden":"scroll"};`
+    links.forEach((link, index)=>{
+        link.style.animation = `linkFade .5s ease forwards ${index/10}s`
+    })
+    menu.classList.toggle("close")
+})
+
+links.forEach((link) => {
+    link.addEventListener("click", ()=>{
+        menu.classList.remove("active")
+        menu.classList.remove("close")
+        container.style = `overflow-y: ${nav.classList.toggle("active")?"hidden":"scroll"};`
+    })
+})
