e1161628b7ae65ad3b263bca197512cfa0de212e
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2021-07-31 14:57:27 +0100

    making progress on the hero section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.querySelectorAll("section").forEach((section, index)=>{
-    section.style.backgroundColor = `rgb(23, 113, 241, ${index*0.15})`
+    section.style.backgroundColor = `rgb(23, 113, 241, ${index*.15})`
 })
 const menu      = document.querySelector("#menu")
 const nav       = document.querySelector(".nav ul")
